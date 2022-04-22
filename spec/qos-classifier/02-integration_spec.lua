a8010b2a91f604da8e38156c78521db53e98708f
Samyak Jain <sjsamyak2001@gmail.com>
2022-04-22 17:13:42 +0530

    Revert "chore: updated tests setup"
    This reverts commit 582ffdb9fadc76880608e7845b73f972f716df26.

--- spec/qos-classifier/02-integration_spec.lua
+++ spec/qos-classifier/02-integration_spec.lua
@@ -68,14 +68,6 @@ for _, strategy in helpers.each_strategy() do
   
     setup(function()
       bp, db = helpers.get_db_utils(strategy, {"plugins"}, { PLUGIN_NAME })
-      
-      -- Start kong
-      assert(helpers.start_kong({
-        database   = strategy,
-        nginx_conf = "/kong-plugin/spec/fixtures/custom_nginx.template",
-        plugins = "bundled, " .. PLUGIN_NAME,
-      }))
-      
       local service = bp.services:insert()
 
       local route1 = bp.routes:insert {
@@ -129,6 +121,13 @@ for _, strategy in helpers.each_strategy() do
         route = route4,
         config = config 
       })
+
+      -- Start kong
+      assert(helpers.start_kong({
+        database   = strategy,
+        nginx_conf = "/kong-plugin/spec/fixtures/custom_nginx.template",
+        plugins = "bundled, " .. PLUGIN_NAME,
+      }))
     end)
 
     teardown(function()
